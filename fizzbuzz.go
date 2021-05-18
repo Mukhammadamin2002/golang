9f5d9b277f69034d1d5d7b5d315efa78d74b6ad7
Mukhammadamin <72244353+Mukhammadamin2002@users.noreply.github.com>
2021-05-18 10:55:10 +0500

    Initial Changes

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -1,24 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	for i := 1; i < 50; i++ {
-		st := ""
-		if i%3 == 0 {
-			st += "Fizz"
-		}
-		if i%5 == 0 {
-			st += "Buzz"
-		}
-		if i%3 == 0 && i%5 {
-			st += "FizzBuzz"
-		}
-		if st != "" {
-			fmt.Println(st)
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("Fizzbuzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
 		} else {
 			fmt.Println(i)
 		}
 
 	}
 }
+
